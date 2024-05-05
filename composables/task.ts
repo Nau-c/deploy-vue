cbcc508b66b6a764eeade662a1e813f4c97f7147
Nauzet Lopez Mendoza <nauzetlm90@gmail.com>
2024-05-05 19:03:45 +0100

    Fixed: test deploy

--- composables/task.ts
+++ composables/task.ts
@@ -1,43 +1,42 @@
 import { defineStore } from "pinia";
-
+// test
 export const useStateTaskStore = defineStore({
-    id: "StateTaskStore",
-    state: () => ({
-        tasks: [] as Task[],
-    }),
-    actions: {
-        addTask(task: string) {
-            let objectTask = {
-                id: Math.random().toString(36).substr(2, 9),
-                timestamp: new Date().getTime(),
-                name: task,
-                completed: false,
-            };
-            this.tasks.push(objectTask);
-        },
-        toggleTask(id: number) {
-            const foundIndex = this.tasks.findIndex((task) => task.id === id);
-            if (foundIndex !== -1) {
-                this.tasks[foundIndex].completed =
-                    !this.tasks[foundIndex].completed;
-            }
-        },
-        removeTask(id: number) {
-            const foundIndex = this.tasks.findIndex((task) => task.id === id);
-            if (foundIndex !== -1) {
-                this.tasks.splice(foundIndex, 1);
-            }
-        },
-        getTasks() {
-            return this.tasks;
-        },
-        editTask(id: number, task) {
-            const foundIndex = this.tasks.findIndex((task) => task.id === id);
-            if (foundIndex !== -1) {
-                this.tasks[foundIndex].name = task.name;
-            }
-        },
+  id: "StateTaskStore",
+  state: () => ({
+    tasks: [] as Task[],
+  }),
+  actions: {
+    addTask(task: string) {
+      let objectTask = {
+        id: Math.random().toString(36).substr(2, 9),
+        timestamp: new Date().getTime(),
+        name: task,
+        completed: false,
+      };
+      this.tasks.push(objectTask);
     },
-    // Persist the store in localStorage
-    persist: true,
+    toggleTask(id: number) {
+      const foundIndex = this.tasks.findIndex((task) => task.id === id);
+      if (foundIndex !== -1) {
+        this.tasks[foundIndex].completed = !this.tasks[foundIndex].completed;
+      }
+    },
+    removeTask(id: number) {
+      const foundIndex = this.tasks.findIndex((task) => task.id === id);
+      if (foundIndex !== -1) {
+        this.tasks.splice(foundIndex, 1);
+      }
+    },
+    getTasks() {
+      return this.tasks;
+    },
+    editTask(id: number, task) {
+      const foundIndex = this.tasks.findIndex((task) => task.id === id);
+      if (foundIndex !== -1) {
+        this.tasks[foundIndex].name = task.name;
+      }
+    },
+  },
+  // Persist the store in localStorage
+  persist: true,
 });
